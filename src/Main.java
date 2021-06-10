bdccfc959da81245c9c84047bd8795c7a7af6b30
poojaghosh <poojaghosh07@gmail.com>
2021-06-10 21:32:22 +0530

    Mary's test2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main{
     public static void main(String args[])throws IOException{
         BufferedReader buf = new BufferedReader(new InputStreamReader(System.in));
         System.out.println("Enter number of seats");
-        Travel t = null;
+		//Travel t = null;
         Map<Integer,Travel> map = new HashMap<Integer,Travel>();
         int id,cid,n = Integer.parseInt(buf.readLine());
         for(int i=0;i<n;i++){
@@ -12,7 +12,7 @@ public class Main{
             id = Integer.parseInt(buf.readLine());
             System.out.println("Enter travel class id "+(i+1));
             cid = Integer.parseInt(buf.readLine());
-            t = new Travel(id,cid);
+          Travel  t = new Travel(id,cid);
             map.put(id,t);
         }
         System.out.println("Total Seat Price Amount: "+t.getSumOfEven(map));
